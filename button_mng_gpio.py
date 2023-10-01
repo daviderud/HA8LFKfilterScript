495fb49e3b2d2f80be2a92ad5624814adf397a73
Davide Rovelli <davide.rovelli@yahoo.com>
2023-10-01 07:19:54 +0200

    added button manager

---
+++ button_mng_gpio.py
@@ -0,0 +1,31 @@
+from gpiozero import Button
+from subprocess import check_call, CalledProcessError
+from signal import pause
+
+def reboot():
+    print("Rebooting...")
+    try:
+        check_call(['/sbin/shutdown', '-r', 'now'])
+    except CalledProcessError as e:
+        # Handle the exception
+        print(f"Command failed with return code {e.returncode}")
+        print(f"Command output: {e.output}")
+        # Perform error handling or recovery actions
+
+def reactivate_usb():
+    print("Reactivating USB...")
+    try:
+        check_call(['sudo', 'uhubctl', '-l', '1-1', '-a', '1'])
+    except CalledProcessError as e:
+        # Handle the exception
+        print(f"Command failed with return code {e.returncode}")
+        print(f"Command output: {e.output}")
+        # Perform error handling or recovery actions
+
+gpio_button = 26
+
+shutdown_btn = Button(gpio_button, hold_time=2)
+shutdown_btn.when_held = reboot
+shutdown_btn.when_pressed = reactivate_usb
+
+pause()
